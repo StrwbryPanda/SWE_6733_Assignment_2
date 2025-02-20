bb663974bc5d4daa820a9567a46658b4972176c4
StrwbryPanda <86024161+StrwbryPanda@users.noreply.github.com>
2025-02-19 19:41:31 -0500

    Mob Session 1
    + Added unit tests
    + Added code coverage screenshot
    + Added test results screenshot
    ~ Edited readme

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -8,11 +8,141 @@ class GildedRoseTest {
 
     @Test
     void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+        Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixthis", app.items[0].name);
+        assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void SulfurasNonZero() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 10, 15)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(10, app.items[0].sellIn);
+        assertEquals(15, app.items[0].quality);
+    }
 
+    @Test
+    void SulfurasZero() {
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(0, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void RandomItem() {
+        Item[] items = new Item[]{new Item("Broadsword", 10, 15)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(9, app.items[0].sellIn);
+        assertEquals(14, app.items[0].quality);
+    }
+
+    @Test
+    void RandomItemZeroDays() {
+        Item[] items = new Item[]{new Item("Broadsword", 0, 15)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieExpiredDays() {
+        Item[] items = new Item[]{new Item("Aged Brie", -3, 15)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-4, app.items[0].sellIn);
+        assertEquals(17, app.items[0].quality);
+    }
+
+    @Test
+    void Backstage() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+    }
+
+    @Test
+    void BackstageThreeDays() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 3, 30)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(2, app.items[0].sellIn);
+        assertEquals(33, app.items[0].quality);
+    }
+
+    @Test
+    void BackstageZeroDays() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 0, 25)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    void BackstageMoreThanTenDays() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 12, 25)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].sellIn);
+        assertEquals(26, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieQualityIncrease() {
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    void AgedBrieQualityNotAbove50() {
+        Item[] items = new Item[]{new Item("Aged Brie", 5, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    void Conjured() {
+        Item[] items = new Item[]{new Item("Conjured", 3, 6)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured", app.items[0].name);
+    }
+
+    @Test
+    void sellInExpired() {
+        Item[] items = new Item[]{new Item("Expired Item", 0, 10)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Expired Item", app.items[0].name);
+    }
+
+    @Test
+    void ItemToString() {
+        Item[] items = new Item[]{new Item("Item", 1, 2)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Item, 0, 1", app.items[0].toString());
+    }
+
+    @Test
+    void QualityLess50(){
+        Item[] items = new Item[]{new Item("Item", 10, 50)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(49,items[0].quality);
+
+    }
 }
+
+
+
--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 # SWE_6673_Assignment_2
 
-## Tools 
+## Tools
 
 **Communication:** Discord  
 **IDE:** IntelliJ IDEA with Code with Me Plugin  
 **Language:** JAVA  
 **Compiler:** Maven  
 **Unit Testing:** JUnit  
-**Code Coverage Tool:** JaCoCo  
+**Code Coverage Tool:** JaCoCo
 
+**Rationale:**  
+Before deciding on our toolset, we met to discuss the group's familiarity with the tools needed to complete the project.
+Most of our group members are familiar with JAVA (or a JAVA-like language like C#), so we
+decided to use JAVA for our project.
 
-## Mob Session #1  
+The source code for the project features a pre-made unit test example using JUnit 5, so we continued to use JUnit
+for the remainder of our unit tests. As for the IDE, one of our group members had the premium version of IntelliJ, which
+allowed for us to mob with our full group (community version only allows for 3 members). Maven was chosen just due to
+familiarity with the compiler. We chose JaCoCo for our code coverage tool after reviewing the links provided from our
+professor. None of us have used code coverage tools for JAVA before, and JaCoCo had a lot of good tutorials for us to
+follow.
+
+Discord was chosen as our form of communication due to group member preference and professor recommendation. Many of us
+are already familiar with the communication tool.
+
+## Mob Session #1
 
 **URL:** 
 
